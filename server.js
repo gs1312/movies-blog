3b3c02b5aae1021673ba0753430dcccb6eb0fc27
Garvit Sharma <53510641+gs1312@users.noreply.github.com>
2020-05-08 13:53:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,12 +32,12 @@ app.get("/ok", (req, res) => {
   res.send("ok");
 });
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+ if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"));
+   app.get("*", (req, res) => {
+     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+ }
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
